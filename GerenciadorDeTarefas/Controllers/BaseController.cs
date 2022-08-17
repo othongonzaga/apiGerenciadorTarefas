dc0ac36abd8c8cc78ebd7044d9ae477bc200de3a
othongonzaga <othoncesar1@hotmail.com>
2022-08-17 15:07:10 -0300

    Mudando controller para deixar metodos privados

---
+++ GerenciadorDeTarefas/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciadorDeTarefas.Controllers
+{
+    [Authorize]
+    public class BaseController : ControllerBase
+    {
+    }
+}
--- GerenciadorDeTarefas/Controllers/LoginController.cs
+++ GerenciadorDeTarefas/Controllers/LoginController.cs
@@ -1,6 +1,7 @@
 ﻿using GerenciadorDeTarefas.Dtos;
 using GerenciadorDeTarefas.Models;
 using GerenciadorDeTarefas.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -10,7 +11,7 @@ namespace GerenciadorDeTarefas.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class LoginController : ControllerBase
+    public class LoginController : BaseController
     {
         private readonly ILogger<LoginController> _logger;
         private readonly string loginTeste = "admin@admin.com";
@@ -22,6 +23,7 @@ namespace GerenciadorDeTarefas.Controllers
         }
 
         [HttpPost]
+        [AllowAnonymous]
         public IActionResult EfetuarLogin([FromBody] LoginRequisicaoDto requisicao)
         {
             try
